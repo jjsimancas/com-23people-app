9eb5291b8bbfbf8df337ecaaead3502d0873fb06
Juan Simancas <jjsimancas@gmail.com>
2020-03-26 04:39:58 -0300

    Se agrega Logging

--- src/main/java/com/people/app/controller/ApiController.java
+++ src/main/java/com/people/app/controller/ApiController.java
@@ -3,6 +3,8 @@ package com.people.app.controller;
 import com.people.app.dto.ErrorDto;
 import com.people.app.model.Course;
 import com.people.app.service.api.CourseInfService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
@@ -23,6 +25,8 @@ public class ApiController {
 
     private final CourseInfService courseInfService;
 
+    private final Logger LOGGER = LoggerFactory.getLogger(ApiController.class);
+
     public ApiController(CourseInfService courseInfService) {
         this.courseInfService = courseInfService;
     }
@@ -30,17 +34,20 @@ public class ApiController {
     @GetMapping(value = "/courses", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public Page<Course> getAllcoursesPage(){
+        LOGGER.info("[Paging Query]");
         return courseInfService.getAllCourseByPage();
     }
 
     @GetMapping(value = "/courses/all", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public List<Course> ListAllcourses(){
+        LOGGER.info("[Listing Courses]");
         return courseInfService.getAllCourses();
     }
 
     @GetMapping(value = "/course/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> getCoursebyId(@PathVariable("id") int id){
+        LOGGER.info("[Query Courses By ID]");
         Course response = courseInfService.getCourseById(id);
         return response == null ? ResponseEntity.status(HttpStatus.NOT_FOUND).body(
                 new ErrorDto(HttpStatus.NOT_FOUND.value(), messageNotfound)): ResponseEntity.ok(response);
@@ -48,6 +55,7 @@ public class ApiController {
 
     @PostMapping(value = "/courses", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> createCourse(@RequestBody Course course)  {
+        LOGGER.info("[Creating Courses]");
         int response = 0;
         try {
             response = courseInfService.createCourse(course);
@@ -60,11 +68,13 @@ public class ApiController {
 
     @PutMapping(value = "/courses/{id}")
     public void updateCourse(@PathVariable("id") int id, @RequestBody Course course){
+        LOGGER.info("[Updating Course]");
         courseInfService.updateCourse(id, course);
     }
 
     @DeleteMapping(value = "/courses/{id}")
     public ResponseEntity<?> deleteCourse(@PathVariable("id") int id){
+        LOGGER.info("[Deleting Course]");
         try{
             courseInfService.deleteCourse(id);
         }catch (Exception ex){
